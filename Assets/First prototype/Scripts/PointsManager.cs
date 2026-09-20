f4fe9df5457166b67c4871c3480db60158bb85be
Johan <johanvdtak@proton.me>
2026-09-20 17:01:50 +0200

    Create pointsManager and debug point input

---
+++ Assets/First prototype/Scripts/PointsManager.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+
+public class PointsManager : MonoBehaviour
+{
+    public int playerAPoints = 0;
+    public int playerBPoints = 0;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        DebugPointsInput();
+    }
+
+
+    private void DebugPointsInput()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                AddPoints(10, "A");
+            } else if (Input.GetKeyDown(KeyCode.B))
+            {
+                AddPoints(10, "B");
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.Alpha2))
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                AddPoints(20, "A");
+            }
+            else if (Input.GetKeyDown(KeyCode.B))
+            {
+                AddPoints(20, "B");
+            }
+        }
+
+        else if (Input.GetKey(KeyCode.Alpha3))
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                AddPoints(30, "A");
+            }
+            else if (Input.GetKeyDown(KeyCode.B))
+            {
+                AddPoints(30, "B");
+            }
+        }
+
+        else return;
+    }
+
+    private void AddPoints(int points, string player)
+    {
+        if (player == "A")
+        {
+            playerAPoints += points;
+            print($"{points} points were added to player {player}");
+            print($"player {player} now has {playerAPoints} points");
+        }
+        else if (player == "B")
+        {
+            playerBPoints += points;
+            print($"{points} points were added to player {player}");
+            print($"player {player} now has {playerBPoints} points");
+        }
+    }
+
+}
